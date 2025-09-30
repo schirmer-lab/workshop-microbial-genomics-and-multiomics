d4c7093f34a984c5557757f572ca42890d34b6ed
AUAShen <71643774+AUAShen@users.noreply.github.com>
2025-09-30 23:46:40 +0200

    updated tutorial and setup configurations

--- mdo_workshop_setup.sh
+++ mdo_workshop_setup.sh
@@ -1,8 +1,8 @@
-micromamba install -y uv
 apt-get update
 apt-get install -f -y python3.12-dev build-essential
 cd Metabiome
-uv sync
+export UV_HTTP_TIMEOUT=3000
+uv sync --active
 uv add ipykernel
 uv run python -m ipykernel install --name metabiome --display-name "Python (Metabiome)"
 
